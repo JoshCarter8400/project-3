374fd98344ad3580aa171eb6656b0ccbbcce84a0
Josh Carter <joshcarter8400@gmail.com>
2021-01-28 20:02:05 -0500

    added in queries for front end

---
+++ client/utils/queries.js
@@ -0,0 +1,83 @@
+import gql from 'graphql-tag';
+
+export const QUERY_SERVICES = gql`
+  query getServices($service: ID) {
+    services {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+    }
+  }
+`;
+
+export const QUERY_ALL_SERVICES = gql`
+  {
+    services {
+      _id
+      name
+      description
+      price
+      quantity
+    }
+  }
+`;
+
+export const QUERY_REVIEWS = gql`
+  query reviews($username: String) {
+    reviews(username: $username) {
+      _id
+      reviewText
+      createdAt
+      username
+    }
+  }
+`;
+
+export const QUERY_REVIEW = gql`
+  query review($id: ID!) {
+    review(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String) {
+    user(username: $username) {
+      _id
+      username
+      email
+      reviews {
+        _id
+        reviewText
+        createdAt
+        orders {
+          _id
+          purchaseDate
+          services {
+            _id
+            name
+            description
+            price
+            quantity
+            image
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($services: [ID]!) {
+    checkout(services: $services) {
+      session
+    }
+  }
+`;
