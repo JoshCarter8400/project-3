8b71b4bd628cbcc15c4d3a19539e0c771a5baa85
Josh Carter <joshcarter8400@gmail.com>
2021-01-28 20:17:06 -0500

    added in front end mutations

---
+++ client/utils/mutations.js
@@ -0,0 +1,50 @@
+import gql from 'graphql-tag';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_ORDER = gql`
+  mutation addOrder($services: [ID]!) {
+    addOrder(services: $services) {
+      purchaseDate
+      services {
+        _id
+        name
+        description
+        price
+        quantity
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_REVIEW = gql`
+  mutation addReview($reviewText: String!) {
+    addReview(reviewText: $reviewText) {
+      _id
+      reviewText
+      createdAt
+      username
+    }
+  }
+`;
