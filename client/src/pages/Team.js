6a95a36e62c0f0df7b4b391903e9049241a55b1c
G.B.Abadio <gilabadio@gmail.com>
2021-01-30 16:42:48 -0500

    added cards for every team member

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -28,38 +28,53 @@ const Team = () => (
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
           </Grid.Column>
-          <Grid.Column floated="right" width={6}>
-          <Card>
-    <Image src='/images/web-application.jpg' wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>Josh</Card.Header>
-      <Card.Meta>
-        <span className='date'>Developing since 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Javescript <br/>
-        Html <br/>
-        CSS <br/>
-        MERN<br/>
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
+        <Grid.Column floated="right" width={6}>
+        <Card>
+        <Image src='/images/web-application.jpg' wrapped ui={false} />
+        <Card.Content>
+            <Card.Header>Josh</Card.Header>
+            <Card.Meta>
+                <span className='date'>Developing since 1931</span>
+            </Card.Meta>
+            <Card.Description>
+                Javescript <br/>
+                Html <br/>
+                CSS <br/>
+                MERN<br/>
+            </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <a href="https://github.com/JoshCarter8400">
+            <Icon name='user' />
+            Github
+             </a>
+        </Card.Content>
+        </Card>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-            <Grid.Column floated="left" width={6}>
-            <Image
-              bordered
-              rounded
-              size="large"
-              src="\images\web-application.jpg"
-            />
+        <Grid.Column floated="left" width={6}>
+        <Card>
+        <Image src='/images/web-application.jpg' wrapped ui={false} />
+        <Card.Content>
+            <Card.Header>Chris</Card.Header>
+            <Card.Meta>
+                <span className='date'>Developing since 131</span>
+            </Card.Meta>
+            <Card.Description>
+                Javescript <br/>
+                Html <br/>
+                CSS <br/>
+                MERN<br/>
+            </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <a href="https://github.com/csouthwick">
+            <Icon name='user' />
+            Github
+             </a>
+        </Card.Content>
+        </Card>
           </Grid.Column>
           <Grid.Column width={8}>
             <Header as="h3" style={{ fontSize: "2em" }}>
@@ -70,7 +85,8 @@ const Team = () => (
             </p>
           </Grid.Column>
           
-        </Grid.Row><Grid.Row>
+        </Grid.Row>
+        <Grid.Row>
           <Grid.Column width={8}>
             <Header as="h3" style={{ fontSize: "2em" }}>
                 Lucas Barros
@@ -80,22 +96,51 @@ const Team = () => (
             </p>
           </Grid.Column>
           <Grid.Column floated="right" width={6}>
-            <Image
-              bordered
-              rounded
-              size="large"
-              src="\images\web-application.jpg"
-            />
+          <Card>
+    <Image src='/images/web-application.jpg' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Lucas</Card.Header>
+      <Card.Meta>
+        <span className='date'>Developing since 2020</span>
+      </Card.Meta>
+      <Card.Description>
+        Javescript <br/>
+        Html <br/>
+        CSS <br/>
+        MERN<br/>
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a href="https://github.com/lucasmbarros">
+        <Icon name='user' />
+        Github
+      </a>
+    </Card.Content>
+  </Card>
           </Grid.Column>
-        </Grid.Row><Grid.Row>
           <Grid.Column floated="left" width={6}>
-            <Image
-              bordered
-              rounded
-              size="large"
-              src="\images\web-application.jpg"
-            />
-          </Grid.Column> 
+        <Card>
+        <Image src='/images/web-application.jpg' wrapped ui={false} />
+        <Card.Content>
+            <Card.Header>Gil</Card.Header>
+            <Card.Meta>
+                <span className='date'>Developing since 2020.5</span>
+            </Card.Meta>
+            <Card.Description>
+                Javescript <br/>
+                Html <br/>
+                CSS <br/>
+                MERN<br/>
+            </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <a href="https://github.com/gilabadio">
+            <Icon name='user' />
+            Github
+             </a>
+        </Card.Content>
+        </Card>
+          </Grid.Column>
           <Grid.Column width={8}>
             <Header as="h3" style={{ fontSize: "2em" }}>
               Gil B. Abadio
