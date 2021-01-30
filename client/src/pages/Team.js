357a89e85d473ec3c03ff911ec080cc8f38d1258
G.B.Abadio <gilabadio@gmail.com>
2021-01-30 16:29:22 -0500

    basic team card completed

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Card,
   Button,
   Container,
   Divider,
@@ -28,12 +29,27 @@ const Team = () => (
             </p>
           </Grid.Column>
           <Grid.Column floated="right" width={6}>
-            <Image
-              bordered
-              rounded
-              size="large"
-              src="\images\web-application.jpg"
-            />
+          <Card>
+    <Image src='/images/web-application.jpg' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Josh</Card.Header>
+      <Card.Meta>
+        <span className='date'>Developing since 2015</span>
+      </Card.Meta>
+      <Card.Description>
+        Javescript <br/>
+        Html <br/>
+        CSS <br/>
+        MERN<br/>
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
