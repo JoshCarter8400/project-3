bb99c482a99bb0965d13bfb2af09f6bf9aa4b5af
Gil B. Abadio <gilabadio@gmail.com>
2021-01-31 18:16:40 -0500

    second cart page

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { removeService,addQuantity,subtractQuantity} from './actions/cartActions'
+import {removeService} from './actions/cartActions'
+
 class Cart extends Component{
 
-    //to remove the service completely
+    //to remove the item completely
     handleRemove = (id)=>{
         this.props.removeService(id);
     }
-    //to add the quantity
-    handleAddQuantity = (id)=>{
-        this.props.addQuantity(id);
-    }
-    //to substruct from the quantity
-    handleSubtractQuantity = (id)=>{
-        this.props.subtractQuantity(id);
-    }
+    
     render(){
               
-        let addedServices = this.props.services.length ?
+        let addedService = this.props.services.length ?
             (  
                 this.props.services.map(service=>{
                     return(
                        
-                        <li className="collection-service avatar" key={service.id}>
+                        <li className="collection-item avatar" key={service.id}>
                                     <div className="service-img"> 
-                                        <img src={service.img} alt={service.img}/>
-                                    </div>                         
+                                        <img src={service.img} alt={service.img} className=""/>
+                                    </div>
+                                
                                     <div className="service-desc">
                                         <span className="title">{service.title}</span>
                                         <p>{service.desc}</p>
@@ -34,13 +29,11 @@ class Cart extends Component{
                                         <p>
                                             <b>Quantity: {service.quantity}</b> 
                                         </p>
-                                        <div className="add-remove">
-                                            <Link to="/cart"><i className="material-icons" onClick={()=>{this.handleAddQuantity(service.id)}}>arrow_drop_up</i></Link>
-                                            <Link to="/cart"><i className="material-icons" onClick={()=>{this.handleSubtractQuantity(service.id)}}>arrow_drop_down</i></Link>
-                                        </div>
-                                        <button className="waves-effect waves-light btn pink remove" onClick={()=>{this.handleRemove(service.id)}}>Remove</button>
-                                    </div>                                
-                                </li>                
+                                        <button className="waves-effect waves-light btn pink remove" onClick={()=>{this.handleRemove(item.id)}}>Remove</button>
+                                    </div>
+                                    
+                                </li>
+                         
                     )
                 })
             ):
@@ -53,23 +46,25 @@ class Cart extends Component{
                 <div className="cart">
                     <h5>You have ordered:</h5>
                     <ul className="collection">
-                        {addedServices}
+                        {addedService}
                     </ul>
                 </div>      
             </div>
        )
     }
 }
+
+
 const mapStateToProps = (state)=>{
     return{
-        services: state.addedServices
+        items: state.addedService,
+        
     }
 }
 const mapDispatchToProps = (dispatch)=>{
     return{
-        removeservice: (id)=>{dispatch(removeService(id))},
+        removeItem: (id)=>{dispatch(removeService(id))},
         addQuantity: (id)=>{dispatch(addQuantity(id))},
-        subtractQuantity: (id)=>{dispatch(subtractQuantity(id))}
     }
 }
 export default connect(mapStateToProps,mapDispatchToProps)(Cart)
