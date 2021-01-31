63a603fc62a4579add866d3de78e4a24bf74681d
Josh Carter <joshcarter8400@gmail.com>
2021-01-30 23:14:00 -0500

    added grid layout for review card and removed unused variables

--- client/src/components/ReviewsList/index.js
+++ client/src/components/ReviewsList/index.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Card } from 'semantic-ui-react';
+import { Card, Link, Grid, Header, Segment } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 const ReviewsList = ({ reviews, title }) => {
   if (!reviews.length) {
-    return <h2>No one has left a review yet</h2>;
+    return <Header as="h2">No one has left a review yet</Header>;
   }
 
   return (
-    <Card>
-      <div>
-        <h2>{title}</h2>
-        {reviews &&
-          reviews.map((review) => (
-            <div key={review._id}>
-              <p>
-                <Link
-                  to={`/profile/${review.username}`}
-                  style={{ fontWeight: 600 }}
-                >
-                  {review.username}
-                </Link>
-                review on {review.createdAt}
-              </p>
-              <div>
-                <p>{review.reviewText}</p>
-              </div>
-            </div>
-          ))}
-      </div>
-    </Card>
+    <div>
+      <Segment>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <Header as="h3" style={{ fontSize: '1em' }}>
+                {title}
+              </Header>
+              {reviews &&
+                reviews.map((review) => (
+                  <Card.Content key={review._id}>
+                    <Card.Header>
+                      <Link
+                        to={`/profile/${review.username}`}
+                        style={{ fontWeight: 600 }}
+                      >
+                        {review.username}
+                      </Link>
+                      review on {review.createdAt}
+                    </Card.Header>
+                    <Card.Description>
+                      <p>{review.reviewText}</p>
+                    </Card.Description>
+                  </Card.Content>
+                ))}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </div>
   );
 };
 
