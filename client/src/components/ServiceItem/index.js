d7faf60c2f347fd382333f8df4bfdd468353f25c
Chris Southwick <csouthwick@live.com>
2021-01-31 21:59:26 -0500

    add service detail page

--- client/src/components/ServiceItem/index.js
+++ client/src/components/ServiceItem/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card, Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 const ServiceItem = ({ service }) => {
   return (
     <Card>
-      <Card.Content key={service._id}>
-        <Image src={require(`../../assets/${service.image}`).default} />
-        <Card.Header style={{marginTop: 1 + "em", textAlign: "center"}}>{service.name}</Card.Header>
-      </Card.Content>
+      <Link to={`/services/${service._id}`}>
+        <Card.Content key={service._id}>
+          <Image src={require(`../../assets/${service.image}`).default} />
+          <Card.Header style={{ marginTop: 1 + "em", textAlign: "center" }}>{service.name}</Card.Header>
+        </Card.Content>
+      </Link>
     </Card>
   );
 };
---
+++ client/src/pages/Detail.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Card,
+  Image,
+  Container,
+  Header,
+  Button
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_SERVICE } from '../utils/queries';
+
+const Detail = () => {
+  const { id: serviceId } = useParams();
+  const { loading, data } = useQuery(QUERY_SERVICE, {
+    variables: { serviceId }
+  });
+
+  const service = data?.service || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Container style={{ textAlign: "center" }}>
+        <Image src={require(`../assets/${service.image}`).default} size="small" centered style={{ marginTop: 2 + "em", marginBottom: 2 + "em" }} />
+        <Header style={{ marginTop: 1 + "em", textAlign: "center" }}>{service.name}</Header>
+        <p>
+          {service.description}
+        </p>
+        <p>
+          Price: ${service.price}
+          <br />
+          <Button primary>Add to cart</Button>
+        </p>
+      </Container>
+      <p style={{ marginTop: 2 + "em", marginBottom: 2 + "em", textAlign: "center" }}>
+        Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </p>
+    </div>
+  );
+};
+
+export default Detail;
