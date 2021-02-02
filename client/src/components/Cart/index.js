e0e1a2e4227f22d76899b13b7775cf08c229835d
Josh Carter <joshcarter8400@gmail.com>
2021-02-02 14:31:33 -0500

    added in cart item, cart components, and installed stripe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,70 +1,113 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import {removeService} from './actions/cartActions'
+import React, { useEffect } from 'react';
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { loadStripe } from '@stripe/stripe-js';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Header, Segment } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+// import {removeService} from './actions/cartActions'
 
-class Cart extends Component{
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
-    //to remove the item completely
-    handleRemove = (id)=>{
-        this.props.removeService(id);
+const Cart = () => {
+  const state = useSelector((state) => {
+    return state;
+  });
+
+  const dispatch = useDispatch();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      dispatch({ type: ADD_MULTIPLE_TO_CART, services: [...cart] });
     }
-    
-    render(){
-              
-        let addedService = this.props.services.length ?
-            (  
-                this.props.services.map(service=>{
-                    return(
-                       
-                        <li className="collection-item avatar" key={service.id}>
-                                    <div className="service-img"> 
-                                        <img src={service.img} alt={service.img} className=""/>
-                                    </div>
-                                
-                                    <div className="service-desc">
-                                        <span className="title">{service.title}</span>
-                                        <p>{service.desc}</p>
-                                        <p><b>Price: {service.price}$</b></p> 
-                                        <p>
-                                            <b>Quantity: {service.quantity}</b> 
-                                        </p>
-                                        <button className="waves-effect waves-light btn pink remove" onClick={()=>{this.handleRemove(item.id)}}>Remove</button>
-                                    </div>
-                                    
-                                </li>
-                         
-                    )
-                })
-            ):
 
-             (
-                <p>Nothing.</p>
-             )
-       return(
-            <div className="container">
-                <div className="cart">
-                    <h5>You have ordered:</h5>
-                    <ul className="collection">
-                        {addedService}
-                    </ul>
-                </div>      
-            </div>
-       )
+    if (!state.cart.length) {
+      getCart();
     }
-}
+  }, [state.cart.length, dispatch]);
 
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
-const mapStateToProps = (state)=>{
-    return{
-        items: state.addedService,
-        
-    }
-}
-const mapDispatchToProps = (dispatch)=>{
-    return{
-        removeItem: (id)=>{dispatch(removeService(id))},
-        addQuantity: (id)=>{dispatch(addQuantity(id))},
-    }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(Cart)
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  }
+
+  function calculateTotal() {
+    let sum = 0;
+    state.cart.forEach((item) => {
+      sum += item.price * item.purchaseQuantity;
+    });
+    return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const serviceIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        serviceIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { services: serviceIds },
+    });
+  }
+
+  if (!state.cartOpen) {
+    return (
+      <Segment onClick={toggleCart}>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
+      </Segment>
+    );
+  }
+
+  return (
+    <Segment>
+      <Segment onClick={toggleCart}>close cart</Segment>
+      <Header as="h2" style={{ fontSize: '1em' }}>
+        Shopping Cart
+      </Header>
+      {state.cart.length ? (
+        <Segment>
+          {state.cart.map((item) => (
+            <CartItem key={item._id} item={item} />
+          ))}
+
+          <Segment>
+            <strong>Total: ${calculateTotal()}</strong>
+
+            {Auth.loggedIn() ? (
+              <Button onClick={submitCheckout}>Checkout</Button>
+            ) : (
+              <span>Please Log In to Checkout</span>
+            )}
+          </Segment>
+        </Segment>
+      ) : (
+        <Header as="h4" style={{ fontSize: '1em' }}>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your Shopping Cart
+        </Header>
+      )}
+    </Segment>
+  );
+};
+
+export default Cart;
