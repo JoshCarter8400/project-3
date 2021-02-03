ba5c959e833b411a78d37e6b6b2521d3ea3e8d56
Lucas Barros <lucasmello.barros@gmail.com>
2021-02-02 19:25:04 -0500

    remove unused imports

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import {
-  Card,
-  Image,
-  Container,
-  Header,
-  Button
-} from "semantic-ui-react";
+import React from "react";
+import { Image, Container, Header, Button } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_SERVICE } from '../utils/queries';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_SERVICE } from "../utils/queries";
 
 const Detail = () => {
   const { id: serviceId } = useParams();
   const { loading, data } = useQuery(QUERY_SERVICE, {
-    variables: { serviceId }
+    variables: { serviceId },
   });
 
   const service = data?.service || {};
@@ -26,19 +20,37 @@ const Detail = () => {
   return (
     <div>
       <Container style={{ textAlign: "center" }}>
-        <Image src={require(`../assets/${service.image}`).default} size="small" centered style={{ marginTop: 2 + "em", marginBottom: 2 + "em" }} />
-        <Header style={{ marginTop: 1 + "em", textAlign: "center" }}>{service.name}</Header>
-        <p>
-          {service.description}
-        </p>
+        <Image
+          src={require(`../assets/${service.image}`).default}
+          size="small"
+          centered
+          style={{ marginTop: 2 + "em", marginBottom: 2 + "em" }}
+        />
+        <Header style={{ marginTop: 1 + "em", textAlign: "center" }}>
+          {service.name}
+        </Header>
+        <p>{service.description}</p>
         <p>
           Price: ${service.price}
           <br />
           <Button primary>Add to cart</Button>
         </p>
       </Container>
-      <p style={{ marginTop: 2 + "em", marginBottom: 2 + "em", textAlign: "center" }}>
-        Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      <p
+        style={{
+          marginTop: 2 + "em",
+          marginBottom: 2 + "em",
+          textAlign: "center",
+        }}
+      >
+        Icons made by{" "}
+        <a href="https://www.freepik.com" title="Freepik">
+          Freepik
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
       </p>
     </div>
   );
